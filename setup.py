b18b5926dc0dfb4367388bc0cfb69d8d6e3f1dd7
mrstephenneal <stephen.neal14@gmail.com>
2018-08-20 16:02:04 -0400

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyPi',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'twine',
+    ],
+    url='https://github.com/mrstephenneal/PyPiDistributor',
+    license='',
+    author='Stephen Neal',
+    author_email='stephen@stephenneal.net',
+    description='Utility for distributing packages to PyPi'
+)
