624ee1660078bd6d57551d7aabb4995c258a40ca
mrstephenneal <stephen.neal14@gmail.com>
2018-08-20 15:59:18 -0400

    Initial Commit

---
+++ distribute.py
@@ -0,0 +1,24 @@
+import os
+from time import sleep
+
+projects = [
+    ('Database Tools', '/Users/Stephen/Dropbox/scripts/dbtools'),
+    ('Loops Tools', '/Users/Stephen/Dropbox/scripts/looptools'),
+    ('PSD Convert', '/Users/Stephen/Dropbox/scripts/psdconvert'),
+    ('Directory Utilities', '/Users/Stephen/Dropbox/scripts/dirutility'),
+    ('PDF Conduit', '/Users/Stephen/Dropbox/scripts/pdfconduit'),
+    ('PyPDF3', '/Users/Stephen/Dropbox/scripts/PyPDF3')
+]
+
+if __name__ == '__main__':
+    print('PyPi distribution control::\n\n' + 'Deploy releases of...')
+
+    for p in projects:
+        name, directory = p
+        update_project = input(name + ' [y/n]: ')
+        if update_project == 'y':
+            os.chdir(directory)
+            os.system('python setup.py sdist')
+            sleep(1)
+            os.system('twine upload -u stephenneal -p pythonstealth19 dist/*')
+            print(name + str(' successfully deployed\n'))
