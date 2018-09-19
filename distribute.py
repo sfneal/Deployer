4ee806787a4021327f562128ce8e9e8c29f780d1
mrstephenneal <stephen.neal14@gmail.com>
2018-09-19 11:24:46 -0400

    Updated PyBundle package name

--- distribute.py
+++ distribute.py
@@ -8,7 +8,7 @@ projects = [
     ('Directory Utilities', '/Users/Stephen/Dropbox/scripts/dirutility'),
     ('PDF Conduit', '/Users/Stephen/Dropbox/scripts/pdfconduit'),
     ('PyPDF3', '/Users/Stephen/Dropbox/scripts/PyPDF3'),
-    ('pybundle', '/Users/Stephen/Dropbox/scripts/PyBundle'),
+    ('PyBundle', '/Users/Stephen/Dropbox/scripts/PyBundle'),
     ('envinfo', '/Users/Stephen/Dropbox/scripts/envinfo'),
 ]
 
