79bc85a00ba52d3d1eea995dfbdcd9e4250fb279
mrstephenneal <stephen.neal14@gmail.com>
2018-10-10 10:46:58 -0400

    Remove redundant full paths so that projects is just flat list of project names

--- PyPiDistr/distribute.py
+++ PyPiDistr/distribute.py
@@ -1,32 +1,34 @@
 import os
 from time import sleep
 
+base_dir = '/Users/Stephen/Dropbox/scripts'
+
 projects = [
-    ('databasetools', '/Users/Stephen/Dropbox/scripts/dbtools'),
-    ('looptools', '/Users/Stephen/Dropbox/scripts/looptools'),
-    ('psdconvert', '/Users/Stephen/Dropbox/scripts/psdconvert'),
-    ('dirutility', '/Users/Stephen/Dropbox/scripts/dirutility'),
-    ('PyPDF3', '/Users/Stephen/Dropbox/scripts/PyPDF3'),
-    ('PyBundle', '/Users/Stephen/Dropbox/scripts/PyBundle'),
-    ('synfo', '/Users/Stephen/Dropbox/scripts/synfo'),
-    ('ImgConverter', '/Users/Stephen/Dropbox/scripts/ImgConverter'),
-    ('pdd_tools3', '/Users/Stephen/Dropbox/scripts/psd-tools3'),
-    ('differentiate', '/Users/Stephen/Dropbox/scripts/differentiate'),
-    ('mysql-toolkit', '/Users/Stephen/Dropbox/scripts/mysql-toolkit'),
+    'databasetools',
+    'looptools',
+    'psdconvert',
+    'dirutility',
+    'PyPDF3',
+    'PyBundle',
+    'synfo',
+    'ImgConverter',
+    'psd-tools3',
+    'differentiate',
+    'mysql-toolkit',
 ]
 
 if __name__ == '__main__':
     print('PyPi distribution control::\n\n' + 'Deploy releases of...')
 
     try:
-        for name, directory in projects:
-            update_project = input(name + ' [y/n]: ')
+        for project in projects:
+            update_project = input(project + ' [y/n]: ')
             if update_project == 'y':
-                os.chdir(directory)
+                os.chdir(os.path.join(base_dir, project))
                 os.system('python setup.py sdist')
                 sleep(1)
                 os.system('twine upload -u stephenneal -p pythonstealth19 dist/*')
-                print(name + str(' successfully deployed\n'))
+                print(project + str(' successfully deployed\n'))
                 exit(0)
     except KeyboardInterrupt:
         exit(0)
