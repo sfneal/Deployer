519a6ae89d8d8c9000f0126af60a5f49d34e8506
mrstephenneal <stephen.neal14@gmail.com>
2018-10-12 11:43:35 -0400

    Added config.py for settings

---
+++ PyPiDistr/config.py
@@ -0,0 +1,19 @@
+BASE_DIR = '/Users/Stephen/Dropbox/scripts'
+
+PROJECTS = [
+    'databasetools',
+    'looptools',
+    'psdconvert',
+    'dirutility',
+    'PyPDF3',
+    'PyBundle',
+    'synfo',
+    'ImgConverter',
+    'psd-tools3',
+    'differentiate',
+    'mysql-toolkit',
+]
+
+USERNAME = 'stephenneal'
+
+PASSWORD = 'pythonstealth19'
