583200116417f123beae6242997f53c361c94c9b
mrstephenneal <stephen.neal14@gmail.com>
2018-10-14 13:21:58 -0400

    Updated to version 0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='DeployPyPi',
-    version='0.2.1',
+    version='0.2.2',
     packages=find_packages(),
     install_requires=[
         'twine>=1.12.1',
