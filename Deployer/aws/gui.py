298c1f404d8ef4c2038c606d121aa00d17d79734
mrstephenneal <stephen.neal14@gmail.com>
2019-03-08 11:35:13 -0500

    REFACTOR whitespace and linebreaks

--- Deployer/aws/gui.py
+++ Deployer/aws/gui.py
@@ -7,6 +7,7 @@ LABEL_COL_WIDTH = 20
 INPUT_COL_WIDTH = 50
 HEADER_FONT_SIZE = 30
 BODY_FONT_SIZE = 20
+DEFAULT_FONT = 'Any {0}'.format(HEADER_FONT_SIZE)
 
 
 def gui(json_path=JSON_PATH, root=ROOT_DIRECTORY):
@@ -17,26 +18,32 @@ def gui(json_path=JSON_PATH, root=ROOT_DIRECTORY):
     sg.SetOptions(text_justification='left')
 
     # Local directory settings
-    directory_settings = [[sg.Text('Root', size=(LABEL_COL_WIDTH, 1), font='Any {0}'.format(BODY_FONT_SIZE)),
-                           sg.In(default_text=root, size=(INPUT_COL_WIDTH, 1), key='root')],
-                          [sg.Text('Source', size=(LABEL_COL_WIDTH, 1), font='Any {0}'.format(BODY_FONT_SIZE)),
-                           sg.In(default_text=most_recent.get('source', None), size=(INPUT_COL_WIDTH, 1),
-                                 key='source')]]
+    directory_settings = [
+        # Root
+        [sg.Text('Root', size=(LABEL_COL_WIDTH, 1), font='Any {0}'.format(BODY_FONT_SIZE)),
+         sg.In(default_text=root, size=(INPUT_COL_WIDTH, 1), key='root')],
+
+        # Source
+        [sg.Text('Source', size=(LABEL_COL_WIDTH, 1), font='Any {0}'.format(BODY_FONT_SIZE)),
+         sg.In(default_text=most_recent.get('source', None), size=(INPUT_COL_WIDTH, 1), key='source')]
+    ]
 
     # DockerHub settings
-    docker_hub_settings = [[sg.Text('Username', size=(LABEL_COL_WIDTH, 1), font='Any {0}'.format(BODY_FONT_SIZE)),
-                            sg.In(default_text=most_recent.get('docker_user', DOCKER_USER), size=(INPUT_COL_WIDTH, 1),
-                                  key='docker_user')],
-                           [sg.Text('Repository', size=(LABEL_COL_WIDTH, 1), font='Any {0}'.format(BODY_FONT_SIZE)),
-                            sg.In(default_text=most_recent.get('docker_repo',
-                                                               most_recent.get('aws_environment-name', '')),
-                                  size=(INPUT_COL_WIDTH, 1),
-                                  key='docker_repo')],
-                           [sg.Text('Tag', size=(LABEL_COL_WIDTH, 1), font='Any {0}'.format(BODY_FONT_SIZE)),
-                            sg.In(default_text=most_recent.get('docker_repo_tag', DOCKER_REPO_TAG),
-                                  size=(INPUT_COL_WIDTH, 1),
-                                  key='docker_repo_tag')]
-                           ]
+    docker_hub_settings = [
+        # Username
+        [sg.Text('Username', size=(LABEL_COL_WIDTH, 1), font='Any {0}'.format(BODY_FONT_SIZE)),
+         sg.In(default_text=most_recent.get('docker_user', DOCKER_USER), size=(INPUT_COL_WIDTH, 1), key='docker_user')],
+
+        # Repo
+        [sg.Text('Repository', size=(LABEL_COL_WIDTH, 1), font='Any {0}'.format(BODY_FONT_SIZE)),
+         sg.In(default_text=most_recent.get('docker_repo', most_recent.get('aws_environment-name', '')),
+               size=(INPUT_COL_WIDTH, 1), key='docker_repo')],
+
+        # Tag
+        [sg.Text('Tag', size=(LABEL_COL_WIDTH, 1), font='Any {0}'.format(BODY_FONT_SIZE)),
+         sg.In(default_text=most_recent.get('docker_repo_tag', DOCKER_REPO_TAG), size=(INPUT_COL_WIDTH, 1),
+               key='docker_repo_tag')]
+    ]
 
     # AWS settings
     aws_settings = [[sg.Text(key[4:len(key)], size=(LABEL_COL_WIDTH, 1), font='Any {0}'.format(BODY_FONT_SIZE)),
@@ -46,15 +53,13 @@ def gui(json_path=JSON_PATH, root=ROOT_DIRECTORY):
                     for key, val in most_recent.items() if key.startswith('aws')]
 
     # Create form layout
-    layout = [[sg.Frame('Directory settings', directory_settings, title_color='green',
-                        font='Any {0}'.format(HEADER_FONT_SIZE))],
-              [sg.Frame('DockerHub settings', docker_hub_settings, title_color='blue',
-                        font='Any {0}'.format(HEADER_FONT_SIZE))],
-              [sg.Frame('AWS Elastic Beanstalk settings', aws_settings, title_color='blue',
-                        font='Any {0}'.format(HEADER_FONT_SIZE))],
-              [sg.Submit(), sg.Cancel()]]
-    window = sg.FlexForm('AWS Elastic Beanstalk Deployment Control',
-                         font=("Helvetica", HEADER_FONT_SIZE)).Layout(layout)
+    layout = [
+        [sg.Frame('Directory settings', directory_settings, title_color='green', font=DEFAULT_FONT)],
+        [sg.Frame('DockerHub settings', docker_hub_settings, title_color='blue', font=DEFAULT_FONT)],
+        [sg.Frame('AWS Elastic Beanstalk settings', aws_settings, title_color='blue', font=DEFAULT_FONT)],
+        [sg.Submit(), sg.Cancel()]]
+    window = sg.FlexForm('AWS Elastic Beanstalk Deployment Control', font=("Helvetica", HEADER_FONT_SIZE))
+    window.Layout(layout)
 
     while True:
         button, values = window.ReadNonBlocking()
