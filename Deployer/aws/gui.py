9c0b144e791bc786b231aae2d0986eb09c528a1f
mrstephenneal <stephen.neal14@gmail.com>
2019-03-08 12:15:48 -0500

    FIX string replacement

--- Deployer/aws/gui.py
+++ Deployer/aws/gui.py
@@ -46,7 +46,7 @@ def gui(json_path=JSON_PATH, root=ROOT_DIRECTORY):
     ]
 
     # AWS settings
-    aws_settings = [[sg.Text(key[4:len(key)].replace('-', '').capitalize(),
+    aws_settings = [[sg.Text(key[4:len(key)].replace('-', ' ').capitalize(),
                              size=(LABEL_COL_WIDTH, 1), font='Any {0}'.format(BODY_FONT_SIZE)),
                      sg.In(default_text=val, size=(INPUT_COL_WIDTH, 1), key=key)]
                     for key, val in most_recent.items() if key.startswith('aws')]
