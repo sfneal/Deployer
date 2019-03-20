4168b8317d80dc167de3f6473ba24b94c38ef00c
mrstephenneal <stephen.neal14@gmail.com>
2019-03-20 11:07:41 -0400

    ADD HOST_PORT & CONTAINER_PORT config values

--- Deployer/aws/config.py
+++ Deployer/aws/config.py
@@ -6,13 +6,13 @@ ECS_HISTORY_JSON = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(
 DOCKER_HISTORY_JSON = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'docker_history.json')
 
 ROOT_DIRECTORY = '/Users/Stephen/Scripts'
-
-DOCKER_USER = 'stephenneal'
-
-DOCKER_REPO_TAG = 'latest'
-
-AWS_REGION = 'us-east-1'
-
 REMOTE_SOURCE_EXT = '-remote'
 
+DOCKER_USER = 'stephenneal'
+DOCKER_REPO_TAG = 'latest'
+
+HOST_PORT = 5000
+CONTAINER_PORT = 5000
+
+AWS_REGION = 'us-east-1'
 LAUNCH_TYPES = ('EC2', 'FARGATE')
