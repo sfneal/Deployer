162e7172db1a9ab328b5897c4acd1c923b3e756d
mrstephenneal <stephen.neal14@gmail.com>
2019-03-20 10:14:20 -0400

    ADD docker_history.json to config

--- Deployer/aws/config.py
+++ Deployer/aws/config.py
@@ -3,6 +3,7 @@ import os
 
 EB_HISTORY_JSON = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'eb_history.json')
 ECS_HISTORY_JSON = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'ecs_history.json')
+DOCKER_HISTORY_JSON = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'docker_history.json')
 
 ROOT_DIRECTORY = '/Users/Stephen/Scripts'
 
