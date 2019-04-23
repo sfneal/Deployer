04c69e8e1c12a3613778d49221c11370c3bc8c7b
mrstephenneal <stephen.neal14@gmail.com>
2019-04-23 15:10:33 -0400

    ADD timer to dockerize script

--- apps/dockerize.py
+++ apps/dockerize.py
@@ -1,31 +1,34 @@
+from looptools import Timer
+
 from Deployer.docker import Docker, gui
 from Deployer.aws.config import DOCKER_HISTORY_JSON
 
 
 def main():
     params = gui()
-    docker = Docker(source=params['source'],
-                    repo=params['docker_repo'],
-                    tag=params['docker_repo_tag'],
-                    username=params['docker_user'],
-                    host_port=params['host_port'],
-                    container_port=params['container_port'])
+    with Timer():
+        docker = Docker(source=params['source'],
+                        repo=params['docker_repo'],
+                        tag=params['docker_repo_tag'],
+                        username=params['docker_user'],
+                        host_port=params['host_port'],
+                        container_port=params['container_port'])
 
-    # Build docker image
-    if params['actions']['build']:
-        docker.build()
+        # Build docker image
+        if params['actions']['build']:
+            docker.build()
 
-    # Push docker image to Docker Hub
-    if params['actions']['push']:
-        docker.push()
+        # Push docker image to Docker Hub
+        if params['actions']['push']:
+            docker.push()
 
-    # Run docker image locally
-    if params['actions']['run']:
-        docker.run()
+        # Run docker image locally
+        if params['actions']['run']:
+            docker.run()
 
-    docker.show_tasks()
-    docker.update_history(DOCKER_HISTORY_JSON, params)
-    print(docker.available_commands)
+        docker.show_tasks()
+        docker.update_history(DOCKER_HISTORY_JSON, params)
+        print(docker.available_commands)
 
 
 if __name__ == '__main__':
