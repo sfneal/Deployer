17e03f6ad055bc3be06591d5c90aaa4e31c97989
mrstephenneal <stephen.neal14@gmail.com>
2019-05-08 11:28:19 -0400

    ADD ability to dockerize multiple applications at once

--- apps/dockerize.py
+++ apps/dockerize.py
@@ -4,36 +4,49 @@ from Deployer.docker import Docker, gui
 from Deployer.aws.config import DOCKER_HISTORY_JSON
 
 
+@Timer.decorator
+def dockerize(params):
+    docker = Docker(source=params['source'],
+                    repo=params['docker_repo'],
+                    tag=params['docker_repo_tag'],
+                    username=params['docker_user'],
+                    host_port=params['host_port'],
+                    container_port=params['container_port'])
+
+    # Build docker image
+    if params['actions']['build']:
+        docker.build()
+
+    # Push docker image to Docker Hub
+    if params['actions']['push']:
+        docker.push()
+    if params['actions']['push-latest']:
+        docker.cmd.tag = 'latest'
+        docker.push()
+        docker.cmd.tag = params['docker_repo_tag']
+
+    # Run docker image locally
+    if params['actions']['run']:
+        docker.run()
+
+    docker.show_tasks()
+    docker.show_commands()
+    docker.update_history(DOCKER_HISTORY_JSON, params)
+    print(docker.available_commands)
+
+
 def main():
-    params = gui()
-    with Timer():
-        docker = Docker(source=params['source'],
-                        repo=params['docker_repo'],
-                        tag=params['docker_repo_tag'],
-                        username=params['docker_user'],
-                        host_port=params['host_port'],
-                        container_port=params['container_port'])
+    parameters = []
+    another = True
+    while another:
+        p = gui()
+        parameters.append(p)
+        if p['another_deploy'] is False:
+            break
 
-        # Build docker image
-        if params['actions']['build']:
-            docker.build()
-
-        # Push docker image to Docker Hub
-        if params['actions']['push']:
-            docker.push()
-        if params['actions']['push-latest']:
-            docker.cmd.tag = 'latest'
-            docker.push()
-            docker.cmd.tag = params['docker_repo_tag']
-
-        # Run docker image locally
-        if params['actions']['run']:
-            docker.run()
-
-        docker.show_tasks()
-        docker.show_commands()
-        docker.update_history(DOCKER_HISTORY_JSON, params)
-        print(docker.available_commands)
+    # Executing Elastic Beanstalk deployments
+    for params in parameters:
+        dockerize(params)
 
 
 if __name__ == '__main__':
