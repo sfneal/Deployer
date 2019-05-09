57ae6784ad7d951c6b074b0410151c411b7452e8
mrstephenneal <stephen.neal14@gmail.com>
2019-05-09 09:21:10 -0400

    ADD get_params function for collecting EB params

--- apps/eb_deploy.py
+++ apps/eb_deploy.py
@@ -2,21 +2,36 @@ from looptools import Timer
 from Deployer.aws.eb import ElasticBeanstalk, gui
 
 
-@Timer.decorator
-def eb_deploy(params):
-    eb = ElasticBeanstalk(source=params['source'],
-                          aws_application_name=params['aws_application-name'],
-                          aws_environment_name=params['aws_environment-name'],
-                          aws_instance_key=params['aws_instance-key'],
-                          aws_version=params['aws_version'],
-                          aws_version_description=params['aws_version-description'],
-                          docker_user=params['docker_user'],
-                          docker_repo=params['docker_repo'],
-                          docker_repo_tag=params['aws_version'],
-                          dockerfile=params['dockerfile'],
-                          host_port=params['host_port'],
-                          container_port=params['container_port'])
+def get_params():
+    """Collect ElasticBeanstalk deployment parameters."""
+    parameters = []
+    another = True
+    while another:
+        p = gui()
+        parameters.append(p)
+        if p['another_deploy'] is False:
+            break
+    return parameters
 
+
+def eb_util_init(params):
+    """Initialize ElasticBeanstalk class and return instance."""
+    return ElasticBeanstalk(source=params['source'],
+                            aws_application_name=params['aws_application-name'],
+                            aws_environment_name=params['aws_environment-name'],
+                            aws_instance_key=params['aws_instance-key'],
+                            aws_version=params['aws_version'],
+                            aws_version_description=params['aws_version-description'],
+                            docker_user=params['docker_user'],
+                            docker_repo=params['docker_repo'],
+                            docker_repo_tag=params['aws_version'],
+                            dockerfile=params['dockerfile'],
+                            host_port=params['host_port'],
+                            container_port=params['container_port'])
+
+
+@Timer.decorator
+def eb_deploy(eb):
     # Ensure directory has been initialized as an Elastic Beanstalk app and that config is correct
     eb.initialize()
 
@@ -31,17 +46,9 @@ def eb_deploy(params):
 
 
 def main():
-    parameters = []
-    another = True
-    while another:
-        p = gui()
-        parameters.append(p)
-        if p['another_deploy'] is False:
-            break
-
     # Executing Elastic Beanstalk deployments
-    for params in parameters:
-        eb_deploy(params)
+    for params in get_params():
+        eb_deploy(eb_util_init(params))
 
 
 if __name__ == '__main__':
