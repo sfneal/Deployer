3d83832ccb4fc3243480cd81a9bec3b50f1f29fb
mrstephenneal <stephen.neal14@gmail.com>
2019-05-29 13:04:13 -0400

    ADD multi-application deployment functionality

--- apps/eb_deploy_multi.py
+++ apps/eb_deploy_multi.py
@@ -5,13 +5,17 @@ from dirutility import SystemCommand
 from Dockerizer.dockerize import main as dockerize
 
 
-def main():
+def gui():
     # Get AWS EB Environment name
     layout = [[sg.Frame('Directory settings',
                         [[sg.Text('AWS EB Env', size=(20, 1), font='Any {0}'.format(20)),
                           sg.In(default_text='', size=(50, 1), key='aws_eb_env')],
                          [sg.Text('Version desc', size=(20, 1), font='Any {0}'.format(20)),
-                          sg.In(default_text='', size=(50, 1), key='desc')]],
+                          sg.In(default_text='', size=(50, 1), key='desc')],
+
+                         # Another deployment?
+                         [sg.Checkbox('Deploy another environment?', size=(50, 1), default=False,
+                                      key='another_deploy')]],
                         title_color='green', font=("Helvetica", 30))],
               [sg.Submit(), sg.Cancel()]]
     window = sg.FlexForm('Docker Hub Deployment Control', font=("Helvetica", 30))
@@ -20,10 +24,12 @@ def main():
     while True:
         button, values = window.ReadNonBlocking()
         if button is 'Submit':
-            break
+            return values
         elif button is 'Cancel':
             exit()
 
+
+def eb_deploy_multi(values):
     # Get Dockerize parameters
     docker = dockerize()[0]
 
@@ -33,6 +39,18 @@ def main():
     with Timer('Deployed to AWS EB'):
         SystemCommand('eb deploy {env} --label {version} --message "{message}"'.format(
             env=values['aws_eb_env'], version=docker.tag, message=values['desc']))
+    return
+
+
+def main():
+    params = []
+    while True:
+        values = gui()
+        params.append(values)
+        if values['another_deploy'] is False:
+            break
+
+    [eb_deploy_multi(value) for value in params]
 
 
 if __name__ == '__main__':
