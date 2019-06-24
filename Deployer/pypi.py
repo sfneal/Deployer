24bcdbe9e568bc58c325174cfe794cbd23f95b95
mrstephenneal <stephen.neal14@gmail.com>
2019-06-24 14:55:11 -0400

    ADD settings directory display to GUI

--- Deployer/pypi.py
+++ Deployer/pypi.py
@@ -20,9 +20,7 @@ config = {
         'PillowImage',
         'RuntimeWatch',
         'Dockerizer',
-    ],
-    'username': 'stephenneal',
-    'password': 'pythonstealth19'
+    ]
 }
 
 
@@ -32,7 +30,7 @@ HEADER_FONT_SIZE = 30
 BODY_FONT_SIZE = 20
 
 
-def gui(projects, default_username='', default_password=''):
+def gui(projects):
     """GUI form for choosing packages to upload to DeployPyPi."""
     sg.SetOptions(text_justification='left')
 
@@ -40,14 +38,11 @@ def gui(projects, default_username='', default_password=''):
     options = [[sg.Checkbox(row, size=(COL_WIDTH, 1), default=False, key=row)] for row in projects]
 
     # DeployPyPi settings
-    settings = [[sg.Text('Username', size=(8, 1), font='Any {0}'.format(BODY_FONT_SIZE)),
-                 sg.In(default_text=default_username, size=(12, 1), key='username')],
-                [sg.Text('Password', size=(8, 1), font='Any {0}'.format(BODY_FONT_SIZE)),
-                 sg.In(default_text=default_password, size=(12, 1), font='Any {0}'.format(BODY_FONT_SIZE),
-                       key='password', password_char='*')]]
+    settings = [[sg.Text('~/.Deployer/pypi.json', font='Any {0}'.format(BODY_FONT_SIZE))]]
 
     # Create form layout
-    layout = [[sg.Frame('PyPi settings', settings, title_color='green', font='Any {0}'.format(HEADER_FONT_SIZE))],
+    layout = [[sg.Frame('Config file', settings, title_color='green', font='Any {0}'.format(
+        HEADER_FONT_SIZE))],
               [sg.Frame('Deployable Projects', options, font='Any {0}'.format(HEADER_FONT_SIZE), title_color='blue')],
               [sg.Submit(), sg.Cancel()]]
     window = sg.FlexForm('DeployPyPi Deployment Control', font=("Helvetica", HEADER_FONT_SIZE)).Layout(layout)
@@ -56,30 +51,26 @@ def gui(projects, default_username='', default_password=''):
         button, values = window.ReadNonBlocking()
 
         if button is 'Submit':
-            # Parse returned values
-            user = values.pop('username')
-            pw = values.pop('password')
-
             # Loop through project options and upload the projects that were checked
             for project, choice in values.items():
                 if choice:
-                    upload(project, user, pw)
+                    upload(project)
             break
         elif button is 'Cancel':
             exit()
 
 
-def upload(project, username, password):
+def upload(project):
     """Upload a package distribution to the DeployPyPi repository."""
     os.chdir(os.path.join(config['directory'], project))
     os.system('python setup.py sdist')
-    command = 'twine upload -u {0} -p {1} dist/*'.format(username, password)
+    command = 'twine upload dist/*'
     os.system(command)
 
 
 def main():
     print('\nDeployPyPi distribution control::\n')
-    gui(config['projects'], config['username'], config['password'])
+    gui(config['projects'])
 
 
 if __name__ == '__main__':
