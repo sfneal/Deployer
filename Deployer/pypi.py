6e4a53f2e4b5f6b2e9ef03d4b4ee89a27933fef0
mrstephenneal <stephen.neal14@gmail.com>
2019-06-24 15:21:19 -0400

    ADD check to make sure JSON file exists and default values

--- Deployer/pypi.py
+++ Deployer/pypi.py
@@ -4,18 +4,31 @@ import PySimpleGUI as sg
 from databasetools import JSON
 
 
+# GUI settings
 TWO_COL = True
 COL_WIDTH = 20
 HEADER_FONT_SIZE = 30
 BODY_FONT_SIZE = 20
 
+# Path to PyPi config file
+PYPI_JSON_PATH = os.path.join(os.path.expanduser('~'), '.Deployer', 'pypi.json')
+PYPI_JSON_DEFAULT = {"projects": [], "username": "", "password": ""}
+
+
+def config_exists():
+    """Determine weather the PyPi config file exists."""
+    return os.path.exists(PYPI_JSON_PATH)
+
+
+def get_config():
+    # Make JSON file if it doesn't exist
+    if not config_exists():
+        JSON(PYPI_JSON_PATH).write(PYPI_JSON_PATH)
+    return JSON(PYPI_JSON_PATH).read()
+
 
 def gui(config):
     sg.SetOptions(text_justification='left')
-
-    # Deployable project options
-    options = [[sg.Checkbox(os.path.basename(row), size=(COL_WIDTH, 1), default=False, key=row)]
-               for row in config['projects']]
 
     # DeployPyPi settings
     settings = [[sg.Text('Username', size=(8, 1), font='Any {0}'.format(BODY_FONT_SIZE)),
@@ -26,16 +39,24 @@ def gui(config):
                 [sg.Text('Config file: ~/.Deployer/pypi.json', font='Any {0}'.format(BODY_FONT_SIZE))]]
 
     # Create form layout
-    layout = [[sg.Frame('PyPi Settings', settings, title_color='green', font='Any {0}'.format(HEADER_FONT_SIZE))],
-              [sg.Frame('Deployable Projects', options, font='Any {0}'.format(HEADER_FONT_SIZE), title_color='blue')],
-              [sg.Submit(), sg.Cancel()]]
+    layout = [[sg.Frame('PyPi Settings', settings, title_color='green', font='Any {0}'.format(HEADER_FONT_SIZE))]]
+
+    # Deployable project options
+    if config_exists():
+        options = [[sg.Checkbox(os.path.basename(row), size=(COL_WIDTH, 1), default=False, key=row)]
+                   for row in config['projects']]
+        layout.extend(
+            [[sg.Frame('Deployable Projects', options, font='Any {0}'.format(HEADER_FONT_SIZE), title_color='blue')]]
+        )
+
+    layout.extend([[sg.Submit(), sg.Cancel()]])
     return sg.FlexForm('DeployPyPi Deployment Control', font=("Helvetica", HEADER_FONT_SIZE)).Layout(layout)
 
 
 def pypi_deployer():
     """GUI form for choosing packages to upload to DeployPyPi."""
     # Read Config file
-    config = JSON(os.path.join(os.path.expanduser('~'), '.Deployer', 'pypi.json')).read()
+    config = get_config()
 
     # Launch GUI
     window = gui(config)
