01a370f3db0b16086746cee66f2594538b2b7369
mrstephenneal <stephen.neal14@gmail.com>
2019-06-24 15:03:24 -0400

    ADD username and password to config file

--- Deployer/pypi.py
+++ Deployer/pypi.py
@@ -3,24 +3,25 @@ import PySimpleGUI as sg
 
 
 config = {
-    'directory': '/Users/Stephen/scripts',
     'projects': [
-        'databasetools',
-        'looptools',
-        'psdconvert',
-        'dirutility',
-        'PyPDF3',
-        'PyBundle',
-        'synfo',
-        'ImgConverter',
-        'psd-tools3',
-        'differentiate',
-        'mysql-toolkit',
-        'awsutils-s3',
-        'PillowImage',
-        'RuntimeWatch',
-        'Dockerizer',
-    ]
+        '/Users/Stephen/scripts/databasetools',
+        '/Users/Stephen/scripts/looptools',
+        '/Users/Stephen/scripts/psdconvert',
+        '/Users/Stephen/scripts/dirutility',
+        '/Users/Stephen/scripts/PyPDF3',
+        '/Users/Stephen/scripts/PyBundle',
+        '/Users/Stephen/scripts/synfo',
+        '/Users/Stephen/scripts/ImgConverter',
+        '/Users/Stephen/scripts/psd-tools3',
+        '/Users/Stephen/scripts/differentiate',
+        '/Users/Stephen/scripts/mysql-toolkit',
+        '/Users/Stephen/scripts/awsutils-s3',
+        '/Users/Stephen/scripts/PillowImage',
+        '/Users/Stephen/scripts/RuntimeWatch',
+        '/Users/Stephen/scripts/Dockerizer',
+    ],
+    'username': 'stephenneal',
+    'password': 'pythonstealth19'
 }
 
 
@@ -30,19 +31,29 @@ HEADER_FONT_SIZE = 30
 BODY_FONT_SIZE = 20
 
 
-def gui(projects):
+def get_config():
+    return config
+
+
+def gui():
     """GUI form for choosing packages to upload to DeployPyPi."""
     sg.SetOptions(text_justification='left')
+    config = get_config()
 
     # Deployable project options
-    options = [[sg.Checkbox(row, size=(COL_WIDTH, 1), default=False, key=row)] for row in projects]
+    options = [[sg.Checkbox(os.path.basename(row), size=(COL_WIDTH, 1), default=False, key=row)]
+               for row in config['projects']]
 
     # DeployPyPi settings
-    settings = [[sg.Text('~/.Deployer/pypi.json', font='Any {0}'.format(BODY_FONT_SIZE))]]
+    settings = [[sg.Text('Username', size=(8, 1), font='Any {0}'.format(BODY_FONT_SIZE)),
+                 sg.In(default_text=config['username'], size=(12, 1), key='username')],
+                [sg.Text('Password', size=(8, 1), font='Any {0}'.format(BODY_FONT_SIZE)),
+                 sg.In(default_text=config['password'], size=(12, 1), font='Any {0}'.format(BODY_FONT_SIZE),
+                       key='password', password_char='*')],
+                [sg.Text('Config file: ~/.Deployer/pypi.json', font='Any {0}'.format(BODY_FONT_SIZE))]]
 
     # Create form layout
-    layout = [[sg.Frame('Config file', settings, title_color='green', font='Any {0}'.format(
-        HEADER_FONT_SIZE))],
+    layout = [[sg.Frame('PyPi Settings', settings, title_color='green', font='Any {0}'.format(HEADER_FONT_SIZE))],
               [sg.Frame('Deployable Projects', options, font='Any {0}'.format(HEADER_FONT_SIZE), title_color='blue')],
               [sg.Submit(), sg.Cancel()]]
     window = sg.FlexForm('DeployPyPi Deployment Control', font=("Helvetica", HEADER_FONT_SIZE)).Layout(layout)
@@ -62,7 +73,7 @@ def gui(projects):
 
 def upload(project):
     """Upload a package distribution to the DeployPyPi repository."""
-    os.chdir(os.path.join(config['directory'], project))
+    os.chdir(os.path.join(os.path.dirname(project), project))
     os.system('python setup.py sdist')
     command = 'twine upload dist/*'
     os.system(command)
@@ -70,7 +81,7 @@ def upload(project):
 
 def main():
     print('\nDeployPyPi distribution control::\n')
-    gui(config['projects'])
+    gui()
 
 
 if __name__ == '__main__':
